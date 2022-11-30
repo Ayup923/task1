73d79141b6c1e39e34de618d9c79407b3df2d008
Ayup923 <aupmaan@gmail.com>
2022-11-30 12:59:33 +0600

    task1

---
+++ shop/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    description = models.TextField()
+    quantity = models.IntegerField()
