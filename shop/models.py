1a7deb9aee86b2aafa7a0d9068514ef137cff399
Aiup <64644481+Ayup923@users.noreply.github.com>
2022-11-30 13:40:35 +0600

    Update models.py

--- shop/models.py
+++ shop/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
-
 class Product(models.Model):
     title = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=7, decimal_places=2)
